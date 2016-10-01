9b7bf149a9cc323147819050ac4c234b976e7d8b
Ghina Dhayne <ghina.d.92@gmail.com>
2016-10-02 00:08:00 +0300

    Add files via upload

---
+++ Login/login.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+
+<body>
+<h1> Login here ! </h1>
+<form action="loginaction.php" method="POST" name="form2">
+	<ul class="list">
+		<li class="item_list"><input type="text" placeholder="Email" name="email" id="email"/></li>
+		<li class="item_list"><input type="password" placeholder="Password" name="pwd" id="pwd"/></li>
+		<li class="item_list"><input type="submit" value="submit"/></li>
+	</ul>
+</form>
+
+</body>
+</html>
---
+++ Login/loginaction.php
@@ -0,0 +1,79 @@
+<?php
+define('DB_SERVER', "localhost");
+define('DB_USER', "root");
+define('DB_PASSWORD', "toor");
+define('DB_DATABASE', "myDB");
+define('DB_DRIVER', "mysql");
+
+$email = $_POST['email'];
+$password = $_POST['pwd'];
+
+try {
+
+    if (empty($_POST["email"]) || empty($_POST["pwd"]) )
+    {
+        $message = "All data are required";
+        echo "<script type='text/javascript'>
+                    alert('$message');
+                    window.location.href='login.php';
+            </script>";
+    }
+
+    else
+    {
+    $conn = new PDO("mysql:host=". DB_SERVER. ";dbname=".DB_DATABASE ,DB_USER, DB_PASSWORD);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "SELECT ClientName, ClientEmail, ClientPwd FROM Client WHERE ClientEmail = :username";
+    $stmt = $conn->prepare($sql);
+    
+    //Bind value.
+    $stmt->bindValue(':username', $email);
+    
+    //Execute.
+    $stmt->execute();
+    
+    //Fetch row.
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    //If $row is FALSE.
+    if($user === false){
+        //Could not find a user with that username!
+        //PS: You might want to handle this error in a more user-friendly manner!
+        $message = "HORA! you are not registred yet ";
+        echo "<script type='text/javascript'>
+                    alert('$message');
+                    window.location.href='signup.php';
+            </script>";
+    } 
+    
+    else
+    {
+        //Compare the passwords.
+
+        if(strcmp($password,$user['ClientPwd']) ==0)
+        {            
+            //Redirect to our protected page, which we called home.php
+            echo "Welcome ". $user['ClientName'];
+            exit;            
+        }        
+        else
+        {
+            //$validPassword was FALSE. Passwords do not match.
+            $message = "Please enter a valid password";
+            echo "<script type='text/javascript'>
+                    alert('$message');
+                    window.location.href='login.php';
+            </script>";
+        }
+    }
+    
+}
+
+}
+catch(PDOException $e) {
+    trigger_error('Error occured while trying to insert into the DB:' . $e->getMessage(), E_USER_ERROR);
+}
+
+?>
